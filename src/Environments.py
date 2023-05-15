d8e08c3dca13f467d4a7ae5eb65e1ff86f896c46
wardgeronimussmets <81919921+wardgeronimussmets@users.noreply.github.com>
2023-05-15 22:27:50 +0200

    no more error in observation space

--- src/Environments.py
+++ src/Environments.py
@@ -1,5 +1,5 @@
 from gym import Env
-from gym.spaces import Box,Tuple,MultiDiscrete
+from gym.spaces import Box,Tuple,MultiDiscrete,Discrete
 import numpy as np
 from mss import mss
 import cv2
@@ -13,6 +13,7 @@ TITLE_COLOR_THRESHOLD = 1500
 PIXEL_COLOR_DIFF_THRESHOLD = 0.3 #factor that it can deviate from the maximum
 WINNER_CHECKER_INTERVAL = 1.8
 OBSERVATION_SHAPE = (3,300,600)
+OBSERVATION_SHAPE_MASK = (3,150,300)
 
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
@@ -22,8 +23,15 @@ class GameEnv(Env):
         super().__init__()
         #setup spaces
         self.observation_shape_channel = OBSERVATION_SHAPE
-        self.observation_shape = (self.observation_shape_channel[2],self.observation_shape_channel[1])
-        self.observation_space = Box(low=0,high=255,shape=self.observation_shape_channel,dtype=np.uint8)
+        self.resize_shape = (self.observation_shape_channel[2],self.observation_shape_channel[1])
+        #multiple observations
+        #first it gets a screencap of the game itself
+        #then a screencap masked with the players at a lower resolution
+        #then it's own color -> hopefully it will learn that and can control multiple colors
+        self.observation_space = Tuple((Box(low=0,high=255,shape=self.observation_shape_channel,dtype=np.uint8), Box(low=0,high=255,shape=OBSERVATION_SHAPE_MASK,dtype=np.uint8),Box(low=0,high=255,shape=(3,1,1),dtype=np.uint8)))
+        # self.observation_space = Tuple((Box(low=0,high=255,shape=self.observation_shape_channel,dtype=np.uint8)),
+        #                                (Box(low=0,high=255,shape=OBSERVATION_SHAPE_MASK,dtype=np.uint8)),
+        #                                (Box(low=0,high=255,shape=(3,1,1),dtype=np.uint8)))
                 
         #action space:
         #   Discretes:
@@ -57,11 +65,16 @@ class GameEnv(Env):
     
     def get_observation(self):
         
-        screenshot = ScreenGrabber.get_resized_screenshot(self.observation_shape)
+        screenshot = ScreenGrabber.get_resized_screenshot(self.resize_shape)
         #Add channels first, this is what stable baselines wants
         channel = np.reshape(screenshot,self.observation_shape_channel)        
         return channel
     
+    
+if __name__ == "__main__":
+    env = GameEnv()
+    print(env.action_space.sample())
+        
 
     
     
